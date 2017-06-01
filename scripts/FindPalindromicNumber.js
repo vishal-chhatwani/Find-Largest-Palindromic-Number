004b5c0b26881ac7b701672d26ff97d08d2d5dcf
vishalchhatwani <vchha001@fiu.edu>
2017-06-01 15:57:20 -0400

    added findPalindromicNumber.index file and its controller

---
+++ scripts/FindPalindromicNumber.js
@@ -0,0 +1,32 @@
+/**
+ * Created by chhatwani on 5/31/2017.
+ */
+
+angular
+    .module('PalindromicNumber')
+    .controller('FindPalindromicNumber',function ($scope) {
+
+        $scope.title= 'Palindromic Number Finder';
+        $scope.findPalindrom = function(){
+
+            var startIndex=$scope.lowerbound;
+            var endIndex=$scope.upperbound;
+            var largestPalindrome=0;
+            for(var i=startIndex;i<=endIndex;i++){
+                var val=i.toString();
+                var reversedValue='';
+                for(j=val.length-1;j>=0;j--){
+                    reversedValue=reversedValue+val.charAt(j);
+                }
+
+                if(reversedValue==val && parseInt(val)>largestPalindrome){
+                        largestPalindrome=parseInt(val);
+                }
+
+            }
+
+            $scope.result=largestPalindrome;
+        }
+
+    });
+
